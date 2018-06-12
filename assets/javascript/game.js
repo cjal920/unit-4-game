2b4f7f1230cfebbbc9e91dcdb2756ba2d2d697bc
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-06-12 11:11:03 -0400

    updating code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
 
     // jQuery methods go here...
- 
+  var numRandom = (Math.floor(Math.random() * 101) + 19);
+
+  $("#target").html(numRandom);
+
+  var numRed = (Math.floor(Math.random() * 12) + 1);
+  var numBlue = (Math.floor(Math.random() * 12) + 1);
+  var numYellow = (Math.floor(Math.random() * 12) + 1);
+  var numGreen = (Math.floor(Math.random() * 12) + 1);
 
   var score = 0; 
-  var wins= 0;
+  var wins = 0;
   var losses = 0;
-
-  
 
 
  
 // pick a random number between 19 and 120. 
 
-var numRandom = (Math.floor(Math.random() * 101) + 19);
-
+function newGame() {
+    numRandom = (Math.floor(Math.random() * 101) + 19);
     $("#target").html(numRandom);
-
     console.log(numRandom);
+    numRed = (Math.floor(Math.random() * 12) + 1);
+    numBlue = (Math.floor(Math.random() * 12) + 1);
+    numYellow = (Math.floor(Math.random() * 12) + 1);
+    numGreen = (Math.floor(Math.random() * 12) + 1);
+    score = 0;
+    $("#score").html(score);
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+    }
+
 
 // assign four random numbers between 1 and 12 to four different colored crystals.
     // pick first random number between 1 and 12. number equals red.
@@ -26,24 +40,98 @@ var numRandom = (Math.floor(Math.random() * 101) + 19);
 
 
     $("#red").on("click", function() {
-        var numRed = (Math.floor(Math.random() * 12) + 1);
-        console.log(numRed);
-        $("#score").text(numRed);
-    });
+        score = numRed + score;
+        console.log("new score:" + score);
+        $("#score").html(score);
+
+        if (score == numRandom) {
+            $("#verdict").html("<h2>YOU WIN<h2>")
+            wins++;
+            $("#wins").html(wins);
+            newGame()
+        }
+
+        else if (score > numRandom) {
+            $("#verdict").html("<h2>YOU LOSE<h2>")
+            losses++;
+            $("#losses").html(losses);
+            newGame()
+        }
+    })
+
+    $("#blue").on("click", function() {
+        score = numBlue + score;
+        console.log("new score:" + score);
+        $("#score").html(score);
+
+        if (score == numRandom) {
+            $("#verdict").html("<h2>YOU WIN<h2>")
+            wins++;
+            $("#wins").html(wins);
+            newGame()
+        }
+
+        else if (score > numRandom) {
+            $("#verdict").html("<h2>YOU LOSE<h2>")
+            losses++;
+            $("#losses").html(losses);
+            newGame()
+        }
+    })
+
+    $("#yellow").on("click", function() {
+        score = numYellow + score;
+        console.log("new score:" + score);
+        $("#score").html(score);
+
+        if (score == numRandom) {
+            $("#verdict").html("<h2>YOU WIN<h2>")
+            wins++;
+            $("#wins").html(wins);
+            newGame()
+        }
+
+        else if (score > numRandom) {
+            $("#verdict").html("<h2>YOU LOSE<h2>")
+            losses++;
+            $("#losses").html(losses);
+            newGame()
+        }
+    })
+
+    $("#green").on("click", function() {
+        score = numGreen + score;
+        console.log("new score:" + score);
+        $("#score").html(score);
+
+        if (score == numRandom) {
+            $("#verdict").html("<h2>YOU WIN<h2>")
+            wins++;
+            $("#wins").html(wins);
+            newGame()
+        }
+
+        else if (score > numRandom) {
+            $("#verdict").html("<h2>YOU LOSE<h2>")
+            losses++;
+            $("#losses").html(losses);
+            newGame()
+        }
+    })
+
+    newGame()
+
+       
+
+
+
+
+
+  /*
     
+    var numRed = (Math.floor(Math.random() * 12) + 1);
 
-
-
-    /*
-    $('#one').on('click', function(){
-        userTotal = userTotal + num1;
-        console.log("New userTotal= " + userTotal);
-        $('#finalTotal').text(userTotal); 
-
-
-*/
-    
-
+    console.log(numRed);
     
     
     // pick second random number between 1 and 12. number equals blue.
@@ -82,6 +170,6 @@ var numRandom = (Math.floor(Math.random() * 101) + 19);
 
 // when score => numRandom, game over. pick new random numbers for 
 
-
+*/
 
 });
