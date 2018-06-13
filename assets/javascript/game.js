5f65d3286fe92c28f41a281e83407d3875877e70
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-06-13 19:47:38 -0400

    updating code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     // jQuery methods go here...
-  var numRandom = (Math.floor(Math.random() * 101) + 19);
+  var numRandom = (Math.floor(Math.random() * 102) + 19);
 
   $("#target").html(numRandom);
 
@@ -9,15 +9,20 @@ $(document).ready(function(){
   var numBlue = (Math.floor(Math.random() * 12) + 1);
   var numYellow = (Math.floor(Math.random() * 12) + 1);
   var numGreen = (Math.floor(Math.random() * 12) + 1);
+  console.log(numRed);
+  console.log(numBlue);
+  console.log(numYellow);
+  console.log(numGreen);
 
   var score = 0; 
   var wins = 0;
   var losses = 0;
 
+
 // pick a random number between 19 and 120. 
 
 function newGame() {
-    numRandom = (Math.floor(Math.random() * 101) + 19);
+    numRandom = (Math.floor(Math.random() * 102) + 19);
     $("#target").html(numRandom);
     console.log(numRandom);
     numRed = (Math.floor(Math.random() * 12) + 1);
@@ -37,7 +42,7 @@ function newGame() {
    // var numRed = (Math.floor(Math.random() * 12) + 1);
 
 
-    $("#red").on("click", function() {
+    $("#ruby").on("click", function() {
         score = numRed + score;
         console.log("new score:" + score);
         $("#score").html(score);
@@ -57,7 +62,7 @@ function newGame() {
         }
     })
 
-    $("#blue").on("click", function() {
+    $("#sapphire").on("click", function() {
         score = numBlue + score;
         console.log("new score:" + score);
         $("#score").html(score);
@@ -77,7 +82,7 @@ function newGame() {
         }
     })
 
-    $("#yellow").on("click", function() {
+    $("#citrine").on("click", function() {
         score = numYellow + score;
         console.log("new score:" + score);
         $("#score").html(score);
@@ -97,7 +102,7 @@ function newGame() {
         }
     })
 
-    $("#green").on("click", function() {
+    $("#emerald").on("click", function() {
         score = numGreen + score;
         console.log("new score:" + score);
         $("#score").html(score);
